c8fe554f1ccde2b5c7ac1e82e550f38ef89f8b99
Mi nombreRodolfo <jesusrlv.informatica@gmail.com>
2021-05-06 15:42:08 -0500

    conn

--- sistema_oic/prcd/conn.php
+++ sistema_oic/prcd/conn.php
@@ -8,9 +8,9 @@
 // $conn= new mysqli ($servername,$username,$password,$database); //solo se quitó para conexión remota
 
 $servername="localhost";
-$database="oic"; //solo se quitó para conexión remota
-$username="root";
-$password="";
+$database="c2oic"; //solo se quitó para conexión remota
+$username="c2cOnf_psic_0307";
+$password="ThmfMTk_G6";
 
 $conn= new mysqli ($servername,$username,$password,$database); //solo se quitó para conexión remota
 
